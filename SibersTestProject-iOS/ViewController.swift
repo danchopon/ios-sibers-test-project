6e872552585f5dd341130b280d2bf18298c09390
Daniyar Erkinov <dan4111k1@gmail.com>
2019-07-03 16:15:51 +0600

    first commit

---
+++ SibersTestProject-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SibersTestProject-iOS
+//
+//  Created by Daniyar Erkinov on 7/3/19.
+//  Copyright © 2019 Daniyar Erkinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
